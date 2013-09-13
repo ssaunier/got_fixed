ce45b95a01d03891ec15d499507259ba3add8121
Sébastien Saunier <seb@saunier.me>
2013-09-13 21:38:04 +0200

    One more spec for IssueFactory#from_github

--- spec/lib/got_fixed/issue_factory_spec.rb
+++ spec/lib/got_fixed/issue_factory_spec.rb
@@ -8,11 +8,19 @@ module GotFixed
 
     describe "#from_github" do
 
-      it "should create an issue from an opened GitHub issue" do
+      it "should create an issue from an opened Github issue" do
         open_issues = JSON.load(File.open "spec/factories/github/open.json")
         issue = @issue_factory.from_github(open_issues.first)
         issue.should_not be_nil
         issue.closed.should be_false
+        issue.title.should eq open_issues.first["title"]
+      end
+
+      it "should create an issue from a closed Github issue" do
+        open_issues = JSON.load(File.open "spec/factories/github/closed.json")
+        issue = @issue_factory.from_github(open_issues.first)
+        issue.should_not be_nil
+        issue.closed.should be_true
         issue.title.should eq open_issues.first["title"]
       end
 
