1b64ad624ea55bce4b638ebd80a85730be5afed6
Sébastien Saunier <seb@saunier.me>
2013-09-13 21:54:41 +0200

    Extend IssueFactory spec with number/vendor_id columns

--- lib/got_fixed/issue_factory.rb
+++ lib/got_fixed/issue_factory.rb
@@ -3,7 +3,9 @@ module GotFixed
     def from_github(github_issue)
       Issue.new \
         :title => github_issue["title"],
-        :closed => github_issue["state"] == "closed"
+        :closed => github_issue["state"] == "closed",
+        :vendor_id => github_issue["id"],
+        :number => github_issue["number"]
     end
   end
 end
--- spec/lib/got_fixed/issue_factory_spec.rb
+++ spec/lib/got_fixed/issue_factory_spec.rb
@@ -13,6 +13,8 @@ module GotFixed
         issue = @issue_factory.from_github(open_issues.first)
         issue.should_not be_nil
         issue.closed.should be_false
+        issue.vendor_id.should eq 3910487
+        issue.number.should eq 1
         issue.title.should eq open_issues.first["title"]
       end
 
@@ -21,6 +23,8 @@ module GotFixed
         issue = @issue_factory.from_github(open_issues.first)
         issue.should_not be_nil
         issue.closed.should be_true
+        issue.vendor_id.should eq 9255225
+        issue.number.should eq 6
         issue.title.should eq open_issues.first["title"]
       end
 
