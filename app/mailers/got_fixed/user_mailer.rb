6e340240d5c35e154f055c85b139bd2be6c35f8d
Sebastien Saunier <seb@saunier.me>
2013-09-22 16:46:16 +0200

    Add a default txt message for the user mailer

--- app/mailers/got_fixed/user_mailer.rb
+++ app/mailers/got_fixed/user_mailer.rb
@@ -3,7 +3,9 @@ module GotFixed
     default from: GotFixed.config[:user_mailer][:from]
 
     def issue_got_fixed_email(user, issue)
-      mail(to: user.email, subject: subject(issue))
+      @user = user
+      @issue = issue
+      mail(to: @user.email, subject: subject(issue))
     end
 
     private
--- spec/mailers/got_fixed/user_mailer_spec.rb
+++ spec/mailers/got_fixed/user_mailer_spec.rb
@@ -6,12 +6,14 @@ module GotFixed
       it "should evaluate template with issue title" do
         @user_mailer = UserMailer.send :new
         issue = FactoryGirl.build :got_fixed_issue, :title => "Dummy issue"
+        old_config = GotFixed.config
         GotFixed.config = {
           :user_mailer => {
             :subject_template => "[Fixed] %{title}"
           }
         }
         @user_mailer.send(:subject, issue).should eq "[Fixed] Dummy issue"
+        GotFixed.config = old_config
       end
     end
   end
