2790311291c867cde42beb25cd4d583bea176413
Sebastien Saunier <seb@saunier.me>
2013-09-29 18:47:22 +0200

    Implement mail sending

--- app/models/got_fixed/issue.rb
+++ app/models/got_fixed/issue.rb
@@ -4,5 +4,19 @@ module GotFixed
     validates_uniqueness_of :vendor_id, :scope => :vendor
 
     has_and_belongs_to_many :issues
+
+    after_update :send_notifications, :if => :just_closed?
+
+    private
+
+      def just_closed?
+        !closed_was && closed
+      end
+
+      def send_notifications
+        issues.each do |issue|
+          issue.send_notification self
+        end
+      end
   end
 end
