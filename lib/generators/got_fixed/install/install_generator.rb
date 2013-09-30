ae0a80710901ffc4179a90724a5df5870c0c950a
Sebastien Saunier <seb@saunier.me>
2013-09-30 12:38:09 +0200

    Installer works

---
+++ lib/generators/got_fixed/install/install_generator.rb
@@ -0,0 +1,19 @@
+module GotFixed
+  module Generators
+    class InstallGenerator < ::Rails::Generators::Base
+
+      desc "Install GotFixed into your app (config + route)"
+
+      source_root File.expand_path("../templates", __FILE__)
+
+      def copy_default_config
+        copy_file "got_fixed.yml", "config/got_fixed.yml"
+      end
+
+      def add_route
+        route "mount GotFixed::Engine => '/got_fixed'"
+      end
+
+    end
+  end
+end
